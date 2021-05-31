e6c4bdf6e1407977eb279b4d6a11d146b330d12a
Manoel Campos <manoelcampos@gmail.com>
2021-05-30 21:59:51 -0300

    Fix test failures after replacing the BATCH_STOCK_QUOTES API by the GLOBAL_QUOTE API one.
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/test/java/org/patriques/StockQuotesTest.java
+++ src/test/java/org/patriques/StockQuotesTest.java
@@ -19,16 +19,22 @@ public class StockQuotesTest {
     String json = "" +
             "{\n" +
             "    \"Meta Data\": {\n" +
-            "        \"1. Information\": \"Batch Stock Market Quotes\",\n" +
+            "        \"1. Information\": \"Stock Market Quotes\",\n" +
             "        \"2. Notes\": \"IEX Real-Time Price provided for free by IEX (https://iextrading.com/developer/).\",\n" +
             "        \"3. Time Zone\": \"US/Eastern\"\n" +
             "    },\n" +
-            "    \"Stock Quotes\": [\n" +
+            "    \"Global Quote\": [\n" +
             "        {\n" +
-            "            \"1. symbol\": \"MSFT\",\n" +
-            "            \"2. price\": \"96.3850\",\n" +
-            "            \"3. volume\": \"987654321\",\n" +
-            "            \"4. timestamp\": \"2018-05-18 15:59:48\"\n" +
+            "            \"01. symbol\": \"MSFT\",\n" +
+            "            \"02. open\": \"250.9950\",\n" +
+            "            \"03. high\": \"252.0800\",\n" +
+            "            \"04. low\": \"249.5600\",\n" +
+            "            \"05. price\": \"96.3850\",\n" +
+            "            \"06. volume\": \"987654321\",\n" +
+            "            \"07. latest trading day\": \"2018-05-18 15:59:48\",\n" +
+            "            \"08. previous close\": \"98.3850\",\n" +
+            "            \"09. change\": \"0.3700\",\n" +
+            "            \"10. change percent\": \"0.1484%\"\n" +
             "        }\n" +
             "    ]\n" +
             "}";
@@ -36,7 +42,7 @@ public class StockQuotesTest {
     StockQuotesResponse resp = stockQuotes.quote("DUMMY");
 
     Map<String, String> metaData = resp.getMetaData();
-    assertThat(metaData.get("1. Information"), is(equalTo("Batch Stock Market Quotes")));
+    assertThat(metaData.get("1. Information"), is(equalTo("Stock Market Quotes")));
     assertThat(metaData.get("2. Notes"), is(equalTo("IEX Real-Time Price provided for free by IEX (https://iextrading.com/developer/).")));
     assertThat(metaData.get("3. Time Zone"), is(equalTo("US/Eastern")));
 
@@ -45,9 +51,15 @@ public class StockQuotesTest {
 
     StockQuote stockQuote = stockData.get(0);
     assertThat(stockQuote.getSymbol(), is(equalTo("MSFT")));
+    assertThat(stockQuote.getOpen(), is(equalTo(250.9950)));
+    assertThat(stockQuote.getHigh(), is(equalTo(252.0800)));
+    assertThat(stockQuote.getLow(), is(equalTo(249.5600)));
     assertThat(stockQuote.getPrice(), is(equalTo(96.3850)));
     assertThat(stockQuote.getVolume(), is(equalTo(987654321L)));
-    assertThat(stockQuote.getTimestamp(), is(equalTo(LocalDateTime.of(2018, 05, 18, 15, 59, 48))));
+    assertThat(stockQuote.getLatestTradingDay(), is(equalTo(LocalDateTime.of(2018, 05, 18, 15, 59, 48))));
+    assertThat(stockQuote.getPreviousClose(), is(equalTo(98.3850)));
+    assertThat(stockQuote.getChange(), is(equalTo(0.3700)));
+    assertThat(stockQuote.getChangePercent(), is(equalTo(0.1484)));
   }
 
   @Test
@@ -55,16 +67,16 @@ public class StockQuotesTest {
     String json = "" +
             "{\n" +
             "    \"Meta Data\": {\n" +
-            "        \"1. Information\": \"Batch Stock Market Quotes\",\n" +
+            "        \"1. Information\": \"Stock Market Quotes\",\n" +
             "        \"2. Notes\": \"IEX Real-Time Price provided for free by IEX (https://iextrading.com/developer/).\",\n" +
             "        \"3. Time Zone\": \"US/Eastern\"\n" +
             "    },\n" +
-            "    \"Stock Quotes\": [\n" +
+            "    \"Global Quote\": [\n" +
             "        {\n" +
-            "            \"1. symbol\": \"MSFT\",\n" +
-            "            \"2. price\": \"96.3850\",\n" +
-            "            \"3. volume\": \"--\",\n" +
-            "            \"4. timestamp\": \"2018-05-18 15:59:48\"\n" +
+            "            \"01. symbol\": \"MSFT\",\n" +
+            "            \"02. price\": \"96.3850\",\n" +
+            "            \"06. volume\": \"--\",\n" +
+            "            \"07. latest trading day\": \"2018-05-18 15:59:48\"\n" +
             "        }\n" +
             "    ]\n" +
             "}";
